889f6e597666ca6f99d88c6a6455c122e79e810e
rruellepetel <ruellepetelromain@gmail.com>
2017-02-13 16:53:02 +0100

    yeeeaaaahhh maaannn

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var decalage = 2;
+var messageCrypte = document.getElementById('messagecrypte');
 
 function crypter(element) {
     var messageClair = document.getElementById('messageclair').value;
     for (var i = 0; i < messageClair.length; i++) {
-        alphabet.indexOf(messageClair)-1;
+        var indexLettreDansAlphabet = alphabet.indexOf(messageClair[i]);
+        var indexLettreDecaleDansAlphabet = indexLettreDansAlphabet + decalage;
+        var lettreDecalee = alphabet[indexLettreDecaleDansAlphabet];
+        messageCrypte.value += lettreDecalee;
     }
-    var messageCrypteElement = document.getElementById('messagecrypte');
-    messageCrypte.value =
 
 }
