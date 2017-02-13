2bb46e7177198bd4c7df9b311142e859c0157c0d
Quentin Gautreau <qgautreau@gmail.com>
2017-02-13 13:54:11 +0100

    on y est presque

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var messageClairElement = document.getElementById('messageclair');
+var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+function crypter(element) {
+    for (var i = 0; i < alphabet.length; i++)
+    
+}
