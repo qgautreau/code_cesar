e496fa5390dd80aa8fbdd9986b6b356169ef71f0
Quentin Gautreau <qgautreau@gmail.com>
2017-02-13 15:53:55 +0100

    eh ben c'est pas pour tout de suite

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var decalage = 2;
-var messageCrypteElement = document.getElementById('messagecrypte').value;
 
 function crypter(element) {
     var messageClair = document.getElementById('messageclair').value;
     for (var i = 0; i < messageClair.length; i++) {
-        messageCrypte.value =
+        alphabet.indexOf(messageClair)-1;
     }
+    var messageCrypteElement = document.getElementById('messagecrypte');
+    messageCrypte.value =
 
 }
