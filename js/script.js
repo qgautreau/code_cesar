90d27dabfde3dfc45d8f0883a16a93bfb62d693f
Quentin Gautreau <qgautreau@gmail.com>
2017-02-13 14:25:29 +0100

    en fait non

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 var messageClairElement = document.getElementById('messageclair');
-var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var alphabet = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var messageCrypteElement = document.getElementById('messagecrypte').value;
 
 function crypter(element) {
     for (var i = 0; i < alphabet.length; i++)
-    
+
 }
